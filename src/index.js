0a6dcf4cff1092f076282702b667a6ba4152cbb0
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-04-29 10:48:45 -0400

    counter

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { render } from 'react-dom';
+import App from './components/App';
+//============my test===============
+
+import {createStore} from 'redux';
+import Counter from './components/Counter';
+import counter from './reducers';
+
+const store = createStore(counter);
+const rootEl = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    rootEl
+  )
+}
+
+render()
+store.subscribe(render);
+
+// render(<App name='World'/>, document.getElementById('root'));
